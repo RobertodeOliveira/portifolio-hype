e34ef58cd0e176734cbf10215787fada823f3d18
Roberto.Melo <robertomelo287@gmail.com>
2024-09-03 11:44:25 -0300

    feat: first  commit, some config project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,20 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{html,js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      screens: {
+        'xl': '1320px',
+      },
+      colors: {
+        'purple': '#7D00FF',
+        'pink': '#ED1CA6',
+        'gray': '#A5A5A5',
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
