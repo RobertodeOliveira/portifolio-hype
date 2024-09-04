9118525e0f382a1ce241aa5c64a8e25d8e898e92
Roberto.Melo <robertomelo287@gmail.com>
2024-09-04 02:39:36 -0300

    feat: develop header in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,8 +17,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={`mx-auto w-[calc(min(1100px,94vw)-2rem)] ${inter.className}`}>
+      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      <body className={`flex flex-col mx-auto w-[calc(min(1352px,94vw)-2rem)] ${inter.className}`}>
+      <header className="h-24 flex justify-end z-10 pt-7">
+        <ul className="flex gap-8">
+          <li><a className="text-menu-header" href="##">ABOUT</a></li>
+          <li><a className="text-menu-header" href="##">SERVICES</a></li>
+          <li><a className="text-menu-header" href="##">WORKD</a></li>
+          <li><a className="text-menu-header" href="##">CONTACT</a></li>
+        </ul>
+      </header>
         {children}
+        <footer className="text-white mb-44">
+          <div className="flex flex-col items-center">
+            <Image src='/image/logo.svg' width={263} height={60} alt="" />
+            <small className="text-sm mt-6">Made with ♥ by Roberto and <span className="text-purple">Hypesoft</span></small>
+          </div>
+        </footer>
       </body>
     </html>
   );
